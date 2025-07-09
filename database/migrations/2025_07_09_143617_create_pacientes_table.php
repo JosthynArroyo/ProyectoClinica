8ab4168d877da51141f43209fcb4b5faf7ed7092
Josthyn Arroyo <josthynarroyo627@gmail.com>
2025-07-09 09:43:46 -0500

    Base de datos

--- database/migrations/2025_07_09_143617_create_pacientes_table.php
+++ database/migrations/2025_07_09_143617_create_pacientes_table.php
@@ -22,4 +22,12 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-}    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
