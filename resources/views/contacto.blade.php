469e883418f6f35fc340cfc7f06dd7a084d1f78e
Josthyn Arroyo <josthynarroyo627@gmail.com>
2025-08-20 09:45:22 -0500

    Creacion de formulario de contacto, y conexion del formulario a la base dedatos, los datos de este formulario se guardan con exito

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -6,7 +6,6 @@
     <title>Formulario de Contacto</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <style>
-        /* Pegamos aquí tu CSS que me diste */
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap');
         :root {
             --clr-primary: #7380ec;
